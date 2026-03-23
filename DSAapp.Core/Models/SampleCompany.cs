bf69ff6c5be3270d00176edf4cd2d40fa435435b
Jesus Garcia <j3susangar1ca@github.com>
2026-03-23 03:48:24 -0600

    t

--- DSAapp.Core/Models/SampleCompany.cs
+++ DSAapp.Core/Models/SampleCompany.cs
@@ -1,60 +1,18 @@
-﻿namespace DSAapp.Core.Models;
+namespace DSAapp.Core.Models;
 
 // Model for the SampleDataService. Replace with your own model.
 public class SampleCompany
 {
-    public string CompanyID
-    {
-        get; set;
-    }
+    public required string CompanyID { get; set; }
+    public required string CompanyName { get; set; }
+    public required string ContactName { get; set; }
+    public required string ContactTitle { get; set; }
+    public required string Address { get; set; }
+    public required string City { get; set; }
+    public required string PostalCode { get; set; }
+    public required string Country { get; set; }
+    public required string Phone { get; set; }
+    public required string Fax { get; set; }
 
-    public string CompanyName
-    {
-        get; set;
-    }
-
-    public string ContactName
-    {
-        get; set;
-    }
-
-    public string ContactTitle
-    {
-        get; set;
-    }
-
-    public string Address
-    {
-        get; set;
-    }
-
-    public string City
-    {
-        get; set;
-    }
-
-    public string PostalCode
-    {
-        get; set;
-    }
-
-    public string Country
-    {
-        get; set;
-    }
-
-    public string Phone
-    {
-        get; set;
-    }
-
-    public string Fax
-    {
-        get; set;
-    }
-
-    public ICollection<SampleOrder> Orders
-    {
-        get; set;
-    }
+    public ICollection<SampleOrder> Orders { get; set; } = new List<SampleOrder>();
 }
